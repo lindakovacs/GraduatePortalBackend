4406c372a1ebf973c786f89028a06ea37f46003b
Matina Patsos <matinaspatsos@gmail.com>
2018-12-22 14:19:53 -0500

    Adding more packages and database configuration

--- config.js
+++ config.js
@@ -8,7 +8,12 @@ require("dotenv").config();
 const { env } = process;
 
 const config = {
-  useCors: env.USE_CORS === "true"
+  useCors: env.USE_CORS === "true",
+  dbHost: env.DB_HOST,
+  dbName: env.DB_NAME,
+  dbUser: env.DB_USERNAME,
+  dbPassword: env.DB_PASSWORD,
+  jwtSecret: env.JWT_SECRET
 };
 
 module.exports = config;
