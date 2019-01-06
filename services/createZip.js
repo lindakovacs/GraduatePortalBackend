d1824654bb58cf5ae8d701c8a79ca962d080cfb6
Matina Patsos <matinaspatsos@gmail.com>
2019-01-06 12:03:50 -0500

    WIP error handling download resumes

--- services/createZip.js
+++ services/createZip.js
@@ -3,47 +3,53 @@ const { join } = require("path");
 const axios = require("axios");
 const archiver = require("archiver");
 
+const zipFilePath = join(`${__dirname}/../AlbanyCanCodeResumes.zip`);
+
 const createZip = urls => {
   return new Promise((resolve, reject) => {
-    const zipFilePath = join(`${__dirname}/../AlbanyCanCodeResumes.zip`);
-    const output = fs.createWriteStream(zipFilePath);
     const archive = archiver("zip", { store: true });
+    const output = fs.createWriteStream(zipFilePath);
+    let numOfFilesWritten = 0;
 
-    try {
-      output.on("close", () => {
-        console.log(archive.pointer() + " total bytes");
-        console.log("File written to", zipFilePath);
-        console.log(
-          "archiver has been finalized and the output file descriptor has closed."
-        );
-        resolve(zipFilePath);
-      });
+    output.on("error", err => {
+      return reject(err);
+    });
 
-      output.on("end", () => {
-        console.log("Data has been drained");
-      });
+    output.on("end", () => {
+      return reject(new Error("Data has been drained"));
+    });
 
-      archive.on("warning", err => {
-        if (err.code === "ENOENT") {
-          // log warning
-          console.log("warning");
-        } else {
-          // throw error
-          throw err;
-        }
-      });
+    output.on("close", () => {
+      // TODO log to server
+      console.log(archive.pointer() + " total bytes");
+      console.log("File written to", zipFilePath);
+      console.log(
+        "archiver has been finalized and the output file descriptor has closed."
+      );
+      return resolve(zipFilePath);
+    });
 
-      archive.pipe(output);
+    archive.on("warning", err => {
+      if (err.code === "ENOENT") {
+        // TODO log
+        console.log("warning");
+      } else return reject(err);
+    });
 
-      archive.on("error", err => {
-        throw err;
-      });
-    } catch (e) {
-      // TODO log
-      console.log(e);
-      reject();
-    }
+    archive.on("error", err => {
+      return reject(err);
+    });
 
+    archive.on("entry", () => {
+      console.log("Achived", urls[numOfFilesWritten]);
+      numOfFilesWritten += 1;
+      if (numOfFilesWritten === urls.length) archive.finalize();
+    });
+
+    archive.pipe(output);
+
+    // setTimeout(() => {
+    // TODO kill setTimeout
     urls.forEach((url, idx) => {
       const pieces = url.split("/");
       const fileName = pieces[pieces.length - 1];
@@ -51,11 +57,9 @@ const createZip = urls => {
         .get(url)
         .then(res => res.data)
         .then(data => archive.append(data, { name: fileName }))
-        .then(() => {
-          if (idx === urls.length - 1) archive.finalize();
-        })
-        .catch(() => reject());
+        .catch(err => reject(err));
     });
+    // }, 2000);
   });
 };
 
