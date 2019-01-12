984705f39064f276fc076a3fe342a4e79e7b0a10
Matina Patsos <matinaspatsos@gmail.com>
2019-01-12 16:21:24 -0500

    Updating with the latest from front-end

---
+++ public/precache-manifest.bc30284915232b37ee9ecaaf032f8046.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "3cbe1d5f79e25df0e7502af5cf55fb52",
+    "url": "/static/media/resume-missing-icon.3cbe1d5f.svg"
+  },
+  {
+    "revision": "cfd04ae5bb9db38e6af505ebe1076f19",
+    "url": "/static/media/resume-icon.cfd04ae5.svg"
+  },
+  {
+    "revision": "dc210b8f690601f03cbea20efbdf6dc0",
+    "url": "/static/media/no-profile.dc210b8f.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "99bac1b761f8be369e8f",
+    "url": "/static/js/main.99bac1b7.chunk.js"
+  },
+  {
+    "revision": "254961e980c6b460b31e",
+    "url": "/static/js/1.254961e9.chunk.js"
+  },
+  {
+    "revision": "99bac1b761f8be369e8f",
+    "url": "/static/css/main.caf6e400.chunk.css"
+  },
+  {
+    "revision": "2b0affe2b80a31ac86e3afc24e2d38ce",
+    "url": "/index.html"
+  }
+];
