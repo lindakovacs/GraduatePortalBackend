7b45845ecf0a92271eae0a548eff1ddc3aecc781
William Peirce <bill@seeyourcampsite.com>
2019-04-22 08:44:42 -0400

    updated models folder

--- models/admin.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const Schema = mongoose.Schema;
-
-const adminSchema = new Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-});
-
-module.exports = mongoose.model("Admin", adminSchema);
