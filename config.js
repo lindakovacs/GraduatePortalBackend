332c10663128d1d5e2ac205e9dcf72fb3596fd91
Louis345 <jt125845@gmail.com>
2019-05-19 14:27:19 -0400

    removed oauth and replaced authentication with an API key

--- config.js
+++ config.js
@@ -14,8 +14,21 @@ const config = {
   awsSecretAccessKey: env.AWS_SECRET_ACCESS_KEY,
   s3UploadRegion: env.S3_UPLOAD_REGION,
   s3UploadBucket: env.S3_UPLOAD_BUCKET,
-  jwtSecret: env.JWT_SECRET
+  jwtSecret: env.JWT_SECRET,
+  googleSheetApiKey: env.GOOGLE_SHEET_API_KEY,
+  googleApiCredentials: {
+    type: "service_account",
+    project_id: env.project_id,
+    private_key_id: env.private_key_id,
+    private_key: env.private_key,
+    client_email: env.client_email,
+    client_id: env.client_id,
+    auth_uri: "https://accounts.google.com/o/oauth2/auth",
+    token_uri: "https://oauth2.googleapis.com/token",
+    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+    client_x509_cert_url:
+      "https://www.googleapis.com/robot/v1/metadata/x509/jamal-236%40albanycancodegraduateportal.iam.gserviceaccount.com"
+  }
 };
 
 module.exports = config;
-
--- routes/data-visualization.js
+++ routes/data-visualization.js
@@ -1,131 +1,46 @@
 const express = require("express");
 const router = express.Router();
 const methodNotAllowed = require("../errors/methodNotAllowed");
-const fs = require("fs");
-const readline = require("readline");
-const { google } = require("googleapis");
+const GoogleSpreadsheet = require("google-spreadsheet");
+const { googleSheetApiKey, googleApiCredentials } = require("../config");
+// Create a document object using the ID of the spreadsheet - obtained from its URL.
 
-// If modifying these scopes, delete token.json.
-const SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"];
-// The file token.json stores the user's access and refresh tokens, and is
-// created automatically when the authorization flow completes for the first
-// time.
-const TOKEN_PATH = "token.json";
 router.get("/", (req, res, next) => {
-  // Load client secrets from a local file.
-  fs.readFile("credentials.json", (err, content) => {
-    if (err) return console.log("Error loading client secret file:", err);
-    // Authorize a client with credentials, then call the Google Sheets API.
-    authorize(JSON.parse(content), readSheet);
+  const doc = new GoogleSpreadsheet(googleSheetApiKey);
+
+  // Authenticate with the Google Spreadsheets API.
+  doc.useServiceAccountAuth(googleApiCredentials, err => {
+    if (err) {
+      return res.status(500).send({
+        isSuccess: 0,
+        err
+      });
+    }
+    doc.getRows(1, (err, rows) => {
+      const tableData = mapData(rows);
+      if (rows.length) {
+        return res.status(200).send({
+          isSuccess: 1,
+          data: tableData
+        });
+      } else {
+        return res.status(500).send({
+          isSuccess: 0,
+          err
+        });
+      }
+    });
   });
 
-  /**
-   * Create an OAuth2 client with the given credentials, and then execute the
-   * given callback function.
-   * @param {Object} credentials The authorization client credentials.
-   * @param {function} callback The callback to call with the authorized client.
-   */
-  function authorize(credentials, callback) {
-    const { client_secret, client_id, redirect_uris } = credentials.installed;
-    const oAuth2Client = new google.auth.OAuth2(
-      client_id,
-      client_secret,
-      redirect_uris[0]
-    );
-
-    // Check if we have previously stored a token.
-    fs.readFile(TOKEN_PATH, (err, token) => {
-      if (err) return getNewToken(oAuth2Client, callback);
-      oAuth2Client.setCredentials(JSON.parse(token));
-      callback(oAuth2Client);
-    });
-  }
-
-  /**
-   * Get and store new token after prompting for user authorization, and then
-   * execute the given callback with the authorized OAuth2 client.
-   * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
-   * @param {getEventsCallback} callback The callback for the authorized client.
-   */
-  function getNewToken(oAuth2Client, callback) {
-    const authUrl = oAuth2Client.generateAuthUrl({
-      access_type: "offline",
-      scope: SCOPES
-    });
-    console.log("Authorize this app by visiting this url:", authUrl);
-    const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout
-    });
-    rl.question("Enter the code from that page here: ", code => {
-      rl.close();
-      oAuth2Client.getToken(code, (err, token) => {
-        if (err)
-          return console.error(
-            "Error while trying to retrieve access token",
-            err
-          );
-        oAuth2Client.setCredentials(token);
-        // Store the token to disk for later program executions
-        fs.writeFile(TOKEN_PATH, JSON.stringify(token), err => {
-          if (err) return console.error(err);
-          console.log("Token stored to", TOKEN_PATH);
-        });
-        callback(oAuth2Client);
-      });
-    });
-  }
-
-  /**
-   *  The Google Sheet that contains AlbanyCanCode Graduates:
-   * @see https://docs.google.com/spreadsheets/d/15l6R1MPpR9QVaey4PstAOLNTNe33ocv01REq8nu9Mi4/edit
-   * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
-   */
-  function readSheet(auth) {
-    const sheets = google.sheets({ version: "v4", auth });
-    sheets.spreadsheets.values.get(
-      {
-        spreadsheetId: "15l6R1MPpR9QVaey4PstAOLNTNe33ocv01REq8nu9Mi4",
-        range: "Sheet1"
-      },
-      (err, response) => {
-        if (err) return console.log("The API returned an error: " + err);
-        const rows = response.data.values;
-        const results = mapData(response.data.values);
-        if (rows.length) {
-          return res.status(200).send({
-            isSuccess: 1,
-            data: results
-          });
-        } else {
-          return res.status(500).send({
-            isSuccess: 0,
-            err
-          });
-        }
-      }
-    );
-  }
-});
-
-mapData = tableData => {
-  const header = tableData[0];
-  const results = tableData
-    .map(data => {
-      const obj = data.reduce((acc, item, index) => {
-        acc[header[index]] = item;
-        return acc;
-      }, {});
+  const mapData = tableData => {
+    const results = tableData.map(data => {
+      const { _links, _xml, save, del, id, app, ...obj } = data;
+      delete obj["app:edited"];
       return obj;
-    })
-    .filter((item, index) => {
-      if (index > 0) {
-        return item;
-      }
     });
-
-  return results;
-};
+    return results;
+  };
+});
 
 router.all("/", methodNotAllowed);
 
